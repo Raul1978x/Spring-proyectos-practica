46a7266c04dbc7fcdd03e2b40762c93c3da73d27
Raul Gomez <gomez.raul.1978.rg@gmail.com>
2023-01-27 17:28:19 -0300

    Proyecto e-commerce iniciado

--- SpringbootProject/src/main/java/com/spring/ecommerce/model/Orden.java
+++ SpringbootProject/src/main/java/com/spring/ecommerce/model/Orden.java
@@ -4,9 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import java.util.Date;
+import java.util.List;
 import lombok.Data;
 
 /**
@@ -16,16 +21,20 @@ import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "ordenes")
 public class Orden {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String numero;
+	private Date fechaCreacion;
+	private Date fechaRecibida;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String numero;
-    @Temporal(TemporalType.DATE)
-    private Date fechaCreacio;
-    @Temporal(TemporalType.DATE)
-    private Date fechaRecibida;
-    private double total;
-    
+	private double total;
+	
+	@ManyToOne
+	private Usuario usuario;
+	
+	@OneToMany(mappedBy = "orden")
+	private List<DetalleOrden> detalle;
 }
--- SpringbootProject/src/main/java/com/spring/ecommerce/model/Usuario.java
+++ SpringbootProject/src/main/java/com/spring/ecommerce/model/Usuario.java
@@ -1,11 +1,37 @@
 package com.spring.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.Data;
+
 /**
  *
  * @author Raúl Gómez
  */
-
-
+@Data
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private String username;
+	private String email;
+	private String direccion;
+	private String telefono;
+	private String tipo;
+	private String password;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> productos;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> ordenes;
 }
